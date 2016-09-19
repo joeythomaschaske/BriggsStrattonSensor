70a1509b4786bcb4e43972f8992362201214a21f
Joseph Thomaschaske <Joseph@Josephs-MacBook-Pro.local>
2016-09-19 00:47:55 -0500

    started on some bluetooth functionality

--- app/src/main/java/com/example/joseph/briggsstrattonapp/MainActivity.java
+++ app/src/main/java/com/example/joseph/briggsstrattonapp/MainActivity.java
@@ -1,15 +1,43 @@
 package com.example.joseph.briggsstrattonapp;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static Button bluetoothConnectButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        init();
         //TEST PUSH KYLE
+    }
+
+    private void init() {
+        bluetoothConnectButton = (Button) findViewById(R.id.bluetoothConnect);
+        bluetoothConnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+                if (mBluetoothAdapter == null) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Bluetooth is not supported on this device :(", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+
+                if (!mBluetoothAdapter.isEnabled()) {
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent, 1);
+                }
+
+            }
+        });
     }
 }

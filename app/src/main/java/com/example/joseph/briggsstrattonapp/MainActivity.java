dbfdfb6e96c4935c22b6d32d959294a16eb01840
kcerfus <kylecer.88@uwm.edu>
2016-10-11 15:35:17 -0500

    Early home page design

--- app/src/main/java/com/example/joseph/briggsstrattonapp/MainActivity.java
+++ app/src/main/java/com/example/joseph/briggsstrattonapp/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.joseph.briggsstrattonapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button Engine_button, Battery_button, Oil_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

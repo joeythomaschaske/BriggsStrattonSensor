4ed9bc3f9b28a0197399d1d7fb65cf79f98fb820
kcerfus <kylecer.88@gmail.com>
2016-10-30 18:45:34 -0500

    linking activities together

--- app/src/main/java/com/example/joseph/briggsstrattonapp/MainActivity.java
+++ app/src/main/java/com/example/joseph/briggsstrattonapp/MainActivity.java
@@ -11,7 +11,7 @@ import android.content.Intent;
 import fragments.TractorFragment;
 
 public class MainActivity extends AppCompatActivity {
-    private Button Engine_button, Battery_button, Oil_button, tractor_button;
+    private Button Engine_button, Battery_button, Oil_button, tractor_button, bluetooth_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,14 +19,15 @@ public class MainActivity extends AppCompatActivity {
         tractor_button = (Button) findViewById(R.id.tractor_button);
         tractor_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String TAG = "MyActivity";
-                Log.v(TAG, "hello world");
-                //Intent intent = new Intent(getApplicationContext(), TractorFragment.class);
-                //startActivity(intent);
-                FragmentTransaction ft = getFragmentManager().beginTransaction();
-                //NEED TO FIX THIS TO START FRAGMENT
-                //ft.add(TractorFragment.newInstance("",""), null);
-                ft.commit();
+                Intent intent = new Intent(getApplicationContext(), TractorActivity.class);
+                startActivity(intent);
+            }
+        });
+        bluetooth_button = (Button) findViewById(R.id.bluetooth_button);
+        bluetooth_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), BluetoothActivity.class);
+                startActivity(intent);
             }
         });
     }

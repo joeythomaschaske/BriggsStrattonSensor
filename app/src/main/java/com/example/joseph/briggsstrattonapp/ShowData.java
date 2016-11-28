b72817e4e52b59c37442564634d0184d712d15b2
kcerfus <kylecer.88@gmail.com>
2016-11-27 20:23:32 -0600

    Made little tweaks

--- app/src/main/java/com/example/joseph/briggsstrattonapp/ShowData.java
+++ app/src/main/java/com/example/joseph/briggsstrattonapp/ShowData.java
@@ -44,8 +44,8 @@ public class ShowData extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_data);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+      //  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+      //  setSupportActionBar(toolbar);
 
        /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
